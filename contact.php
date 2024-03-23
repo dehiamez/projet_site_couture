c6ad945b0db18a8fc8be10a66cb415d5e0fb7b39
dehiamez <125209390+dehiamez@users.noreply.github.com>
2024-03-23 15:54:10 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,9 @@
-  <!--cinquieme partie-->
     <div class="container">
         <div class="row">
             <div class="col-lg-12" id="partiedecontact">
                 <h3>Contact</h3>
                 <label for="name">Nom et Prénom :</label>
-                <input type="text">
+                <input type="text" id="name">
                 <br>
                 <label for="numero">Numéro de téléphone :</label>
                 <input type="number" name="numero" id="numero">
@@ -14,10 +13,7 @@
                 <br>
                 <p>Posez votre question :</p>
                 <textarea name="question" id="question" cols="40" rows="10"></textarea>
-                
-
             </div>
-
         </div>
     </div>
 
