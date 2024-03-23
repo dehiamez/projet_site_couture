3efe98c7a029bee4702e86ce9ac0236dcfd1ceef
Rajaayamoul <164520723+Rajaayamoul@users.noreply.github.com>
2024-03-23 14:13:06 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,23 @@
+  <!--cinquieme partie-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" id="partiedecontact">
+                <h3>Contact</h3>
+                <label for="name">Nom et Prénom :</label>
+                <input type="text">
+                <br>
+                <label for="numero">Numéro de téléphone :</label>
+                <input type="number" name="numero" id="numero">
+                <br>
+                <label for="email">Email :</label>
+                <input type="email" name="email" id="email">
+                <br>
+                <p>Posez votre question :</p>
+                <textarea name="question" id="question" cols="40" rows="10"></textarea>
+                
+
+            </div>
+
+        </div>
+    </div>
 
