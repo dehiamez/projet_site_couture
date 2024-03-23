85f8dd010802d005c31723247ec8f2adf169b544
dehiamez <125209390+dehiamez@users.noreply.github.com>
2024-03-23 17:17:17 +0000

    Update

--- contact.php
+++ contact.php
@@ -1,33 +1,34 @@
     <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap.css" />
-	<link rel="stylesheet" href="coutureforyou.css" />
-	<link rel="icon" href="logo_couture.jpg" type="image/x-icon" />
-    <title>Contact - COUTUREFORYOU</title>
-</head>
-	<?php include ("nav_couture.php") ?>
+    <html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap.css" />
+        <link rel="stylesheet" href="coutureforyou.css" />
+        <link rel="icon" href="logo_couture.jpg" type="image/x-icon" />
+        <title>Contact - COUTUREFORYOU</title>
+    </head>
+    <?php include ("nav_couture.php") ?>
+
     <body>
         <div class="container">
-        <div class="row">
-            <div class="col-lg-12" id="partiedecontact">
-                <h3>Contact</h3>
-                <label for="name">Nom et Prénom :</label>
-                <input type="text" id="name">
-                <br>
-                <label for="numero">Numéro de téléphone :</label>
-                <input type="number" name="numero" id="numero">
-                <br>
-                <label for="email">Email :</label>
-                <input type="email" name="email" id="email">
-                <br>
-                <p>Posez votre question :</p>
-                <textarea name="question" id="question" cols="40" rows="10"></textarea>
+            <div class="row">
+                <div class="col-lg-12" id="partiedecontact">
+                    <h3>Contact</h3>
+                    <label for="name">Nom et Prénom :</label>
+                    <input type="text" id="name">
+                    <br>
+                    <label for="numero">Numéro de téléphone :</label>
+                    <input type="number" name="numero" id="numero">
+                    <br>
+                    <label for="email">Email :</label>
+                    <input type="email" name="email" id="email">
+                    <br>
+                    <p>Posez votre question :</p>
+                    <textarea name="question" id="question" cols="40" rows="10"></textarea>
+                </div>
             </div>
         </div>
-    </div>
-	    <?php include ("footer_couture.php")?>
-</body>
-
+        <?php include ("footer_couture.php")?>
+    </body>
--- page_de_login.php
+++ page_de_login.php
@@ -1,91 +1,59 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Login - COUTUREFORYOU</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - COUTUREFORYOU</title>
 </head>
+
 <body>
-	<?php include ("nav_couture.php") ?>
-	<div class="container">
-		<div class="row">
-					<div class="col-sm-6" id="leftpart">
-						<form method="post" action="#">
-							<h2>Se connecter</h2>
-							<label for="email"
-								>Adresse Email:</label
-							>
-							<br />
-							<input
-								type="email"
-								name="email"
-								id="email"
-							/>
-							<br />
-							<label for="mdp"
-								>Mot de passe :</label
-							>
-							<br />
-							<input
-								type="password"
-								name="mot de passe"
-								id="mdp"
-							/>
-							<br />
-							<input
-								type="button"
-								value="Se connecter"
-								name="connect"
-								class="connect_button"
-							/>
-						</form>
-					</div>
-					<div class="col-sm-6" id="rightpart">
-						<form>
-						<h2>Créer un nouveau compte</h2>
-						<label for="nom">Nom :</label>
-						<br />
-						<input type="text" name="nom" id="nom" />
-						<br />
-						<label for="prenom">Prénom :</label>
-						<br />
-						<input
-							type="text"
-							name="prenom"
-							id="prenom"
-						/>
-						<br />
-						<label for="birthdate"
-							>Date de Naissance :</label
-						>
-						<br />
-						<input
-							type="date"
-							name="birthdate"
-							id="birthdate"
-						/>
-						<br />
-						<label for="email">Adresse Email :</label>
-						<br />
-						<input type="email" />
-						<br />
-						<label for="password">Mot de passe:</label>
-						<br />
-						<input
-							type="password"
-							id="password"
-							name="psswrd"
-						/><br />
-						<input
-							type="button"
-							value="S'inscrire"
-							class="connect_button"
-						/>
-						</form>
-					</div>
-				</div>
-			</div>
-			            <?php include("footer_couture.php")?>
+    <?php include ("nav_couture.php") ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6" id="leftpart">
+                <form method="post" action="#">
+                    <h2>Se connecter</h2>
+                    <label for="email">Adresse Email:</label>
+                    <br />
+                    <input type="email" name="email" id="email" />
+                    <br />
+                    <label for="mdp">Mot de passe :</label>
+                    <br />
+                    <input type="password" name="mot de passe" id="mdp" />
+                    <br />
+                    <input type="button" value="Se connecter" name="connect" class="connect_button" />
+                </form>
+            </div>
+            <div class="col-sm-6" id="rightpart">
+                <form>
+                    <h2>Créer un nouveau compte</h2>
+                    <label for="nom">Nom :</label>
+                    <br />
+                    <input type="text" name="nom" id="nom" />
+                    <br />
+                    <label for="prenom">Prénom :</label>
+                    <br />
+                    <input type="text" name="prenom" id="prenom" />
+                    <br />
+                    <label for="birthdate">Date de Naissance :</label>
+                    <br />
+                    <input type="date" name="birthdate" id="birthdate" />
+                    <br />
+                    <label for="email">Adresse Email :</label>
+                    <br />
+                    <input type="email" />
+                    <br />
+                    <label for="password">Mot de passe:</label>
+                    <br />
+                    <input type="password" id="password" name="psswrd" /><br />
+                    <input type="button" value="S'inscrire" class="connect_button" />
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php include("footer_couture.php")?>
 
 </body>
+
 </html>
