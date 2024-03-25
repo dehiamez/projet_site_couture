94b43f9bd878d128d8718c5eb30de8b5aa6d8b34
dehiamez <125209390+dehiamez@users.noreply.github.com>
2024-03-25 09:41:28 +0100

    update

--- contact_couture.php
+++ contact_couture.php
@@ -16,6 +16,7 @@
             <div class="row">
                 <div class="col-lg-12" id="partiedecontact">
                     <h3>Contact</h3>
+                    <form action="" method="POST">
                     <label for="name">Nom et Prénom :</label>
                     <input type="text" id="name">
                     <br>
@@ -27,6 +28,7 @@
                     <br>
                     <p>Posez votre question :</p>
                     <textarea name="question" id="question" cols="40" rows="10"></textarea>
+                    </form>
                 </div>
             </div>
         </div>
