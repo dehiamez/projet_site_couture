f1e288aa7730ea7caa832a22d589fd290a9770d2
dehiamez <mezianedehia@gmail.com>
2024-04-05 10:48:30 +0200

    update

--- devis_couture.php
+++ devis_couture.php
@@ -95,32 +95,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["devis"])) {
     $couleur = $_POST["couleur"];
     $service = $_POST["service"];
 
-    // Construire le contenu de l'e-mail
-    $subject = "Récapitulatif de votre devis";
-    $message = "Voici le récapitulatif de votre devis :\n\n";
-    $message .= "Description : $description\n";
-    $message .= "Type de vêtements : $vetements\n";
-    $message .= "Type de tissu : $tissu\n";
-    $message .= "Taille : $taille\n";
-    $message .= "Couleur : $couleur\n";
-    $message .= "Services : $service\n";
-
+    
     // Adresse e-mail du client
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']===true){
         $email_client = $_SESSION['email'];
+
+        // Construire le contenu de l'e-mail
+        $subject = "Récapitulatif de votre devis";
+        $message = "Voici le récapitulatif de votre devis :\n\n";
+        $message .= "Description : $description\n";
+        $message .= "Type de vêtements : $vetements\n";
+        $message .= "Type de tissu : $tissu\n";
+        $message .= "Taille : $taille\n";
+        $message .= "Couleur : $couleur\n";
+        $message .= "Services : $service\n";
+
+        // Envoi de l'e-mail au client
+        $headers = "From : coutureForFun@mail.com" ;
+        $headers.="Content-Type : text/html;charset=utf-8\r\n";
+        
+        if(mail($email_client, $subject, $message, $headers)){
+            // Confirmation d'envoi
+            echo "Le récapitulatif du devis a été envoyé à $email_client.";
+        }
+        else{
+            echo"erreur lors de l'envoi à $email_client."
+;       }
     }
     else{
         $bouton_login = '<button class="btn btn-outline-primary" onclick="window.location.href=\'page_de_login.php\'">Me connecter pour accéder aux inscriptions</button>';
         echo $bouton_login;
     }
 
-    // Envoi de l'e-mail au client
-    $from = "coutureForFun@mail.com";
-    $headers = "From:" . $from;
-    mail($email_client, $subject, $message, $headers);
-
-    // Confirmation d'envoi
-    echo "Le récapitulatif du devis a été envoyé à $email_client.";
+    
 }
 ?>
 
