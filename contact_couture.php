b09b87f10820aecc6254c66b88d9b02e868c8b37
dehiamez <mezianedehia@gmail.com>
2024-04-07 21:16:44 +0200

    update

--- contact_couture.php
+++ contact_couture.php
@@ -17,25 +17,31 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-lg-12" id="partiedecontact">
-                <form action="#" method="POST" class="form-control">
-                    <h3>Contact</h3>
-                    <label for="name">Nom et Prénom :</label>
-                    <input type="text" id="name" class="form-control" pattern="[A-Za-z\ ]+" required>
-                    <br>
-                    <label for="numero">Numéro de téléphone :</label>
-                    <input type="tel" name="numero" id="numero" class="form-control" pattern="[0-9]{10}" required>
-                    <br>
-                    <label for="email">Email :</label>
-                    <input type="email" name="email" id="email" class="form-control" required>
-                    <br>
-                    <p>Posez votre question :</p>
-                    <textarea name="question" id="question" cols="40" rows="10" class="form-control"
-                        required></textarea><br>
-                    <input type="submit" value="Soumettre ma demande" class="btn btn-outline-primary">
-                </form>
-            </div>
+            <form action="#mailto:coutureForYou@mail.com" method="POST" class="form-control">
+                <h3>Contact</h3>
+                <label for="name">Nom et Prénom :</label>
+                <input type="text" id="name" class="form-control" pattern="[A-Za-z\ ]+" required>
+                <br>
+                <label for="numero">Numéro de téléphone :</label>
+                <input type="tel" name="numero" id="numero" class="form-control" pattern="[0-9]{10}" required>
+                <br>
+                <label for="email">Email :</label>
+                <input type="email" name="email" id="email" class="form-control" required>
+                <br>
+                <p>Posez votre question :</p>
+                <textarea name="question" id="question" cols="40" rows="10" class="form-control"
+                    required></textarea><br>
+                <input type="submit" value="Soumettre ma demande" class="btn btn-outline-primary">
+            </form>
+        </div>
+    </div>
+    <div class="contact container">
+        <div class="row">
+            <h6>Pour nous contacter : </h6>
+            <p>Numero de téléphone : +33 9 46 34 75 28 | Adresse mail : coutureForYou@mail.com</p>
         </div>
     </div>
 </body>
 <?php include ("footer_couture.php")?>
+
+</html>
